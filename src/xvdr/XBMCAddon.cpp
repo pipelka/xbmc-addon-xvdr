9b350a4d4b7f5291cc1564a7f774d72565be4ccd
Alexander Pipelka <alexander.pipelka@gmail.com>
2012-10-12 11:04:53 +0200

    added seek / pause for recordings

--- src/xvdr/XBMCAddon.cpp
+++ src/xvdr/XBMCAddon.cpp
@@ -641,6 +641,28 @@ long long LengthRecordedStream(void)
   return 0;
 }
 
+bool CanPauseStream()
+{
+  mCallbacks->Lock();
+  bool rc = (mDemuxer == NULL);
+  mCallbacks->Unlock();
+
+  return rc;
+}
+
+bool CanSeekStream()
+{
+  mCallbacks->Lock();
+  bool rc = (mDemuxer == NULL);
+  mCallbacks->Unlock();
+
+  return rc;
+}
+
+void PauseStream(bool bPaused)
+{
+}
+
 const char* GetPVRAPIVersion(void)
 {
   static const char *strApiVersion = XBMC_PVR_API_VERSION;
@@ -669,7 +691,4 @@ PVR_ERROR SetRecordingPlayCount(const PVR_RECORDING &recording, int count) { ret
 PVR_ERROR SetRecordingLastPlayedPosition(const PVR_RECORDING &recording, int lastplayedposition) { return PVR_ERROR_NOT_IMPLEMENTED; }
 int GetRecordingLastPlayedPosition(const PVR_RECORDING &recording) { return -1; }
 unsigned int GetChannelSwitchDelay(void) { return 0; }
-bool CanPauseStream() { return false; }
-bool CanSeekStream() { return false; }
-void PauseStream(bool bPaused) {}
 }
