216305501f8bd136e6f91181cd29fc66f4c8f2f3
Alexander Pipelka <alexander.pipelka@gmail.com>
2012-11-07 13:46:59 +0100

    autogen.sh: check for needed binaries
    - this maybe fixes native MacOS X compilation, as
      libtoolize is missing (AFAIK, there is a glibtoolize
      binary)

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,61 @@
+found_bin=""
+
+checkbin() {
+  echo -n "Checking for $1 ... "
+  found_bin=`which $1`
+
+  if [ "x$found_bin" != "x" ] ; then
+    echo $found_bin
+    return 0
+  fi
+
+  echo "not found !"
+  return 1
+}
+
+# check for libtoolize
+
+checkbin libtoolize || {
+  checkbin glibtoolize || {
+    echo "neither \"libtoolize\" nor \"glibtoolize\" found !"
+    exit 1
+  }
+}
+LIBTOOLIZE=$found_bin
+
+# check for aclocal
+
+checkbin aclocal || {
+  exit 1
+}
+ACLOCAL=$found_bin
+
+# check for automake
+
+checkbin automake || {
+  exit 1
+}
+AUTOMAKE=$found_bin
+
+# check for autoconf
+
+checkbin autoconf || {
+  exit 1
+}
+AUTOCONF=$found_bin
+
+echo
+
 echo "Generating build information ..."
 cd `dirname $0`
 aclocalinclude="$ACLOCAL_FLAGS"
 mkdir -p autotools
 
 echo "Running libtoolize ..."
-libtoolize --copy --force --automake || exit 1
+$LIBTOOLIZE --copy --force --automake || exit 1
 
 echo "Running aclocal $aclocalinclude ..."
-aclocal $aclocalinclude || {
+$ACLOCAL $aclocalinclude || {
     echo
     echo "**Error**: aclocal failed. This may mean that you have not"
     echo "installed all of the packages you need, or you may need to"
@@ -18,10 +66,10 @@ aclocal $aclocalinclude || {
 }
 
 echo "Running automake ..."
-automake -c -a --foreign || ( echo "***ERROR*** automake failed." ; exit 1 )
+$AUTOMAKE -c -a --foreign || ( echo "***ERROR*** automake failed." ; exit 1 )
 
 echo "Running autoconf ..."
-autoconf || ( echo "***ERROR*** autoconf failed." ; exit 1 )
+$AUTOCONF || ( echo "***ERROR*** autoconf failed." ; exit 1 )
 
 echo
 echo "Please run ./configure now."
