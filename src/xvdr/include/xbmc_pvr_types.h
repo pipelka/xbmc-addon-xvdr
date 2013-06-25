68986825b7d3ea51a7bbb97a30fa08c842d70e18
Daniel Meyerholt <dxm523@gmail.com>
2013-06-25 15:59:43 +0200

    Fixed MinGW32 compile thx to pipelkas comments by reverting some defines

--- src/xvdr/include/xbmc_pvr_types.h
+++ src/xvdr/include/xbmc_pvr_types.h
@@ -52,7 +52,7 @@ struct DemuxPacket;
 #undef PRAGMA_PACK_BEGIN
 #undef PRAGMA_PACK_END
 
-#if defined(__GNUC__)
+#if defined(__GNUC__) && !defined(__MINGW32__)
 #if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 95)
 #define ATTRIBUTE_PACKED __attribute__ ((packed))
 #define PRAGMA_PACK 0
